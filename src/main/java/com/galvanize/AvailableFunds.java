a158e8cc99f6a527365646189954eafaca1e7c50
Castillo <905903@cognizant.com>
2021-05-01 10:16:46 -0500

    Source code & test checking to see if funds available

---
+++ src/main/java/com/galvanize/AvailableFunds.java
@@ -0,0 +1,9 @@
+package com.galvanize;
+
+public class AvailableFunds {
+    private int funds = 500000;
+
+    public int showBalance() {
+        return funds;
+    }
+}
--- src/test/java/com/galvanize/LenderTest.java
+++ src/test/java/com/galvanize/LenderTest.java
@@ -1,4 +1,35 @@
 package com.galvanize;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LenderTest {
+
+
+    //As a lender, I want to be able to check my available funds,
+    //so that I know how much money I can offer for loans.
+
+    //When I check my available funds
+    //Then I should see how much funds I currently have
+    AvailableFunds availableFunds;
+
+    @BeforeEach
+    void setUp() {
+        availableFunds = new AvailableFunds();
+    }
+
+    @Test
+    public void testMyFundsShowAvailableBalance() {
+        // Arrange
+
+
+        // Act
+         int fundsAvailable = availableFunds.showBalance();
+
+        // Assert
+        assertTrue(fundsAvailable > 0);
+
+    }
 }
